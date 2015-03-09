a4c1a08fa09946293ad242583b28ad7b241837b5
Christopher Foo <chris.foo@gmail.com>
2015-03-09 17:34:12 -0400

    pipeline: Point to production url

--- pipeline.py
+++ pipeline.py
@@ -57,11 +57,10 @@ if not WGET_LUA:
 #
 # Update this each time you make a non-cosmetic change.
 # It will be added to the WARC files and reported to the tracker.
-VERSION = "20150300.01"
+VERSION = "20150309.01"
 USER_AGENT = 'ArchiveTeam'
 TRACKER_ID = 'google-business-sitebuilder'
-# TRACKER_HOST = 'tracker.archiveteam.org'
-TRACKER_HOST = 'localhost:9080'
+TRACKER_HOST = 'tracker.archiveteam.org'
 
 
 ###########################################################################
